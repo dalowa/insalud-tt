7c9991151b6dacc9a18bdc8e2e4372f09bddb7b5
dalowa <davidurbano.dev@gmail.com>
2025-09-27 09:07:11 -0500

    allow new images urls

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[
+      new URL('https://flagcdn.com/w320/**'), 
+      new URL('https://flagcdn.com/**'), 
+      new URL('https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/**')],
+  }
 };
 
 export default nextConfig;
+
+
