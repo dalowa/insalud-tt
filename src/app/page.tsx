ed29ef53b26db5b9dc3cb80ef7b88b79d043c705
dalowa <davidurbano.dev@gmail.com>
2025-09-28 14:31:02 -0500

    clean main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { httpClientPlugin } from "@/lib/http-client.plugin";
 import { CountryCardsDisplay } from "@/components/CountryCardsDisplay";
-import { Country } from "@/types/countriesAPI-type";
+
 
 
 export default async function Home() {
-  const URL_COUNTRIES_API = 'https://restcountries.com/v3.1/all?fields=name,cca3,flags,region,population,capital';
-
-  const countriesList: Country[] = await httpClientPlugin.get(URL_COUNTRIES_API);
-  
-  console.log(new Set(countriesList.map(e => e.region)))
   return (
     
     <div className="font-sans flex flex-col min-h-screen pb-20 gap-16 sm:p-20">
