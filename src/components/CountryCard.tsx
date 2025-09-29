8307a0b120433f94fc30014f98530ffad59b10b4
dalowa <davidurbano.dev@gmail.com>
2025-09-29 08:59:43 -0500

    now much better ui

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -15,14 +15,13 @@ interface CountryCardProps {
 export function CountryCard({country, setModalIsOpen, setCurrentModalCountry} : CountryCardProps) {
 
    const handleClickModal = (c: Country) => {
-      console.log("Click detected")
-      
+      /* console.log("Click detected") */
       setModalIsOpen(true)
       setCurrentModalCountry(c)
    }
   return (
     <div
-      className={`border border-black w-40 h-70 md:w-52 xl:w-64 xl:h-[28rem] flex flex-col gap-3 bg-gray-300 rounded-2xl cursor-pointer`}
+      className={`border border-black w-40 h-70 md:w-52 xl:w-64 xl:h-[28rem] flex flex-col gap-3 bg-gray-400/50 rounded-2xl cursor-pointer`}
       onClick={() => handleClickModal(country)}
    >
       <h3 className="rounded-t-2xl font-bold text-center bg-black text-white py-2 leading-4 px-0.5 xl:py-5 xl:text-2xl">{country.name.common}</h3>
