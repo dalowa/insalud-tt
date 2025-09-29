bdd6805b416ab52550c1bdd48044b1f89c4d15ca
dalowa <davidurbano.dev@gmail.com>
2025-09-28 21:55:53 -0500

    this just get the countries list and create a loading state

---
+++ src/hooks/useCountries.ts
@@ -0,0 +1,25 @@
+import { httpClientPlugin } from '@/lib/http-client.plugin';
+import { Country } from '@/types/countriesAPI-type';
+import { useEffect, useState } from 'react';
+export const useCountries = () => {
+   const [allCountries, setAllCountries] = useState<Country[]>([]);
+   const [loading, setLoading] = useState(true);
+
+   useEffect( () => {
+      const requestCountriesList = async () => {  // Función DENTRO
+         try {
+            setLoading(true);
+            const URL_COUNTRIES_API = 'https://restcountries.com/v3.1/all?fields=name,cca3,flags,region,population,capital';
+            const countriesList = await httpClientPlugin.get(URL_COUNTRIES_API);
+            setAllCountries(countriesList);
+         } catch (error) {
+            console.error('Error:', error);
+         } finally {
+            setLoading(false);
+         }
+      };
+
+      requestCountriesList();
+   },[])
+   return { allCountries, loading };
+};
