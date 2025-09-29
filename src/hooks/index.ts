68a430d9037f6a6fe210b6c8df44c028cf7a007c
dalowa <davidurbano.dev@gmail.com>
2025-09-29 09:12:31 -0500

    new usePagination hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { usePagination } from './usePagination';
 export { useModalCountry } from './useModalCountry';
 export { useCountries } from "./useCountries";
 export { useCountryFilters } from "./useCountryFilters";
