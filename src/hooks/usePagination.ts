b6e31cf9d848ad324fffdb063b06b12fc03cc3f1
dalowa <davidurbano.dev@gmail.com>
2025-09-29 09:12:50 -0500

    all pagination logic is here

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,31 @@
+import { Country } from "@/types/countriesAPI-type";
+import { useEffect, useState } from "react";
+
+export const usePagination = (itemsPerPage: number, countries:Country[]) => {
+    const [currentPage, setCurrentPage] = useState(1) 
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentItems = countries.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(countries.length / itemsPerPage);
+
+   const handlePrevious = () => {
+      if (currentPage > 1) {
+      setCurrentPage(prev => prev - 1);
+      }
+   };
+
+   const handleNext = () => {
+      if (currentPage < totalPages) {
+      setCurrentPage(prev => prev + 1);
+      }
+   };
+
+   useEffect(() => {
+      setCurrentPage(1);
+   }, [countries.length]);
+    return {
+      currentItems,
+      handlePrevious,
+      handleNext
+    }
+}
