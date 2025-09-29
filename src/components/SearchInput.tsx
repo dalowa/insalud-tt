68c10268097040b87aa8063ec5171ba8d8fdbc39
dalowa <davidurbano.dev@gmail.com>
2025-09-28 21:50:28 -0500

    only search bar

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Input } from './ui/input'
+
+interface SearchInputProps {
+   inputSearch: string;
+   setInputSearch: (value: string) => void;
+}
+
+export const SearchInput = ({inputSearch, setInputSearch}:SearchInputProps) => {
+  return (
+      <label className='flex bg-black w-full justify-center px-5 py-2 gap-2'>
+         <Input 
+            type="text" 
+            className='bg-white text-black ' 
+            value={inputSearch} 
+            onChange={e => setInputSearch(e.target.value)} 
+            /* onKeyDown={e => e.key === 'Enter' && onClickSearch()} */
+            title='Search for country'/>
+      </label>
+  )
+}
