7de4527637e34d12ae87a409f9ca2cb1790c3c61
dalowa <davidurbano.dev@gmail.com>
2025-09-29 07:14:35 -0500

    ui improved

--- src/components/PopulationRange.tsx
+++ src/components/PopulationRange.tsx
@@ -9,10 +9,11 @@ interface PopulationRangeProps {
 
 export const PopulationRange = ({minPopulation, maxPopulation, setMaxPopulation, setMinPopulation}:PopulationRangeProps) => {
   return (
-      <div className="flex gap-2 items-center w-">
+      <div className="flex gap-2 items-center bg-black w-full lg:w-[30%] text-white justify-center mb-4 sm:w-[45%] ">
             <label className='flex flex-col gap-2'>
                <p>Min Poblacion:</p>
-               <input 
+               <input
+                  className='bg-white text-black rounded-2xl px-2 py-1'
                   type="number" 
                   value={minPopulation} 
                   onChange={e => setMinPopulation(Number(e.target.value))}
@@ -23,7 +24,8 @@ export const PopulationRange = ({minPopulation, maxPopulation, setMaxPopulation,
             </label>
             <label className='flex flex-col gap-2'>
                <p>Max Poblacion:</p>
-               <input 
+               <input
+                  className='bg-white text-black rounded-2xl px-2 py-1'
                   type="number" 
                   value={maxPopulation} 
                   onChange={e => setMaxPopulation(Number(e.target.value))}
@@ -31,9 +33,7 @@ export const PopulationRange = ({minPopulation, maxPopulation, setMaxPopulation,
                   max={1000000000}
                   placeholder='Min poblacion'
                />
-            </label>
-            
-             
+            </label>   
         </div>
   )
 }
