d46529537bfa78af1294a727954f3681d3cfec7c
dalowa <davidurbano.dev@gmail.com>
2025-09-29 07:08:21 -0500

    improved ui and new button to fav page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { CountryCardsDisplay } from "@/components/CountryCardsDisplay";
+import Link from "next/link";
 
 
 
 export default async function Home() {
   return (
     
-    <div className="font-sans flex flex-col min-h-screen pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-col min-h-screen pb-20 pt-10 gap-5 lg:gap-16">
       <h1 className="text-4xl sm:text-6xl font-bold text-center"> Countries of the World </h1>
-      
+      <Link className="bg-amber-300 py-3 px-6 w-44 text-center rounded-3xl font-bold mx-auto" href={"/favorites"}>Go to Favorites</Link>
       <CountryCardsDisplay />
+      
     </div>
   );
 }
