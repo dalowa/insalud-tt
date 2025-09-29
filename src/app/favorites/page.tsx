92495b0f3c6e721d6622bac1b63763923f190690
dalowa <davidurbano.dev@gmail.com>
2025-09-29 07:07:22 -0500

    display favorites countries from local storage

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { DisplayFavorites } from '@/components/DisplayFavorites';
+import Link from 'next/link';
+import React from 'react';
 
-export const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
-  )
+    <div className='font-sans flex flex-col min-h-screen pb-20 pt-10 gap-5 lg:gap-16'>
+      <h1 className="text-4xl sm:text-6xl font-bold text-center"> Favorites Countries of the World</h1>
+      <Link className="bg-green-400 py-3 px-6 w-44 text-center rounded-3xl font-bold mx-auto" href={"/"}>Go to Home</Link>
+      <DisplayFavorites />
+    </div>
+  );
 }
+
+export default Page;
+
