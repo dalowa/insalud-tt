b8f43b7bb4ca2ffae94d75bdd3006a8a608d61d9
dalowa <davidurbano.dev@gmail.com>
2025-09-29 07:15:45 -0500

    ui improved

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -8,7 +8,7 @@ interface SearchInputProps {
 
 export const SearchInput = ({inputSearch, setInputSearch}:SearchInputProps) => {
   return (
-      <label className='flex bg-black w-full justify-center px-5 py-2 gap-2'>
+      <label className='flex bg-black w-full justify-center px-5 py-2 gap-2 lg:w-[30%]'>
          <Input 
             type="text" 
             className='bg-white text-black ' 
