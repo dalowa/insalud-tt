7a365356a7e5e8090b4f4ef9c20eb098d9ee6b3c
dalowa <davidurbano.dev@gmail.com>
2025-09-29 12:30:17 -0500

    now only handle the state

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -1,4 +1,4 @@
-import { httpClientPlugin } from '@/lib/http-client.plugin';
+import { countryService } from '@/services/countryService';
 import { Country } from '@/types/countriesAPI-type';
 import { useEffect, useState } from 'react';
 export const useCountries = () => {
@@ -8,8 +8,8 @@ export const useCountries = () => {
       const requestCountriesList = async () => {
          try {
             setLoading(true);
-            const URL_COUNTRIES_API = 'https://restcountries.com/v3.1/all?fields=name,cca3,flags,region,population,capital';
-            const countriesList = await httpClientPlugin.get(URL_COUNTRIES_API);
+            
+            const countriesList = await countryService.getAllCountries();
             setAllCountries(countriesList);
          } catch (error) {
             console.error('Error:', error);
