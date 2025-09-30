979fce52c5d9bd27407cf0f54a9b49b6af82f88e
dalowa <davidurbano.dev@gmail.com>
2025-09-30 16:20:17 -0500

    new way to handle the numbers format

---
+++ src/lib/utils/format.ts
@@ -0,0 +1,10 @@
+export function formatPopulation(population: number): string {
+  if (population >= 1000000000) {
+    return (population / 1000000000).toFixed(1) + 'B';
+  } else if (population >= 1000000) {
+    return (population / 1000000).toFixed(1) + 'M';
+  } else if (population >= 1000) {
+    return (population / 1000).toFixed(1) + 'K';
+  }
+  return population.toString();
+}
