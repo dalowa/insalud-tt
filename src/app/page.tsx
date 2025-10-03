0f2b0ab02dc25b6e7fcd820c2cc4a67f2991eccd
dalowa <davidurbano.dev@gmail.com>
2025-10-03 08:45:36 -0500

    style: fix indentation in main page component
    - Correct spacing and formatting in Home component
    - Improve code readability and consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 
 import { CountryCardsDisplay } from "@/components/country";
 
-
-
 export default async function Home() {
   return (
     
-    <div className="font-sans flex flex-col min-h-screen pb-20 pt-10 gap-5 lg:gap-16">
-      
+    <div className="font-sans flex flex-col min-h-screen max-w-[var(--max-w-container)] gap-5 lg:gap-16">    
       <CountryCardsDisplay />
-      
     </div>
   );
 }
