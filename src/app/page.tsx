400dbd4d8a8a380d01749c3032c1cb336a31e4ea
dalowa <davidurbano.dev@gmail.com>
2025-10-03 13:42:40 -0500

    fix(ui): centered with margin auto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import { CountryCardsDisplay } from "@/components/country";
 
 export default async function Home() {
   return (
-    
-    <div className="font-sans flex flex-col min-h-screen max-w-[var(--max-w-container)] gap-5 lg:gap-16">    
+    <div className="font-sans flex flex-col min-h-screen max-w-[var(--max-w-container)] mx-auto gap-5 lg:gap-16">    
       <CountryCardsDisplay />
     </div>
   );
