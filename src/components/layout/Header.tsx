46019a796846982a0a9ad5e1c4eae3b6b24a4140
dalowa <davidurbano.dev@gmail.com>
2025-10-02 21:49:16 -0500

    feat(ui): almost done, just few buttons needs to work still

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { IconWorld } from '../icons/IconWorld';
 import { IconExchange } from '../icons/IconExchange';
-import Link from 'next/link';
 import { CountryFilters } from '../country';
+import { NavBar } from './NavBar';
 
 export const Header = () => {
-
-
   return (
-    <header className='px-3 py-5 flex flex-col'>
+    <header className='px-3 py-5 flex flex-col sm:px-5 max-w-[var(--max-w-container)] mx-auto'>
       <div className='flex justify-between'>
         <h1 className='flex justify-center items-center gap-2'>
           <span>
@@ -17,15 +15,12 @@ export const Header = () => {
           <p className='font-bold text-xl text-[1.35rem]'>Country Explorer</p>
         </h1>
         <div>
-          <nav className='flex flex-col font-semibold border-gray-500 border rounded-lg overflow-hidden'>
-            <Link className={`bg-red-400 text-white text-center px-3 py-[0.25rem]`} href={'/'}>Home</Link>
-            <Link className={`text-center px-3 py-[0.25rem]`} href={'/favorites'}>Favorites</Link>
-          </nav>
+          <NavBar />
         </div>
       </div>
 
       <div>
-        <div className='flex justify-center mt-4 mb-2'>
+        <div className='flex justify-center sm:justify-start mt-4 mb-2'>
           <button className='bg-white  text-gray-700 border-[1px] flex justify-center items-center gap-1 border-gray-500 px-2 py-1 rounded-lg' type='button'>
             <span>
               <IconExchange className='w-4 h-4 ' />
