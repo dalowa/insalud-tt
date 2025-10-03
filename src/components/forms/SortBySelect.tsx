5719e74d5c6ff588013a2336d009e2a1f9c77176
dalowa <davidurbano.dev@gmail.com>
2025-10-02 21:40:40 -0500

    feat(filter): new filter for countries with names and population asc/desc

---
+++ src/components/forms/SortBySelect.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '../ui';
+import { useCountriesStore } from '@/store';
+
+export const SortBySelect = () => {
+   const { setOrderByCode } = useCountriesStore()
+
+   return (
+         <label className='flex justify-center flex-col w-[30%]' >
+            <Select onValueChange={e => setOrderByCode(Number(e))}>
+               <SelectTrigger className="w-full bg-white text-black border border-black ">
+                  <SelectValue className='text-black placeholder:text-black placeholder:text-4xl' placeholder="Order by" />
+               </SelectTrigger>
+               <SelectContent>
+                  <SelectGroup>
+                     <SelectLabel>Order by</SelectLabel>
+                     <SelectItem value="0">Default</SelectItem>
+                     <SelectItem value="1">Name (A → Z)</SelectItem>
+                     <SelectItem value="2">Name (Z → A)</SelectItem>
+                     <SelectItem value="3">Population (Ascending)</SelectItem>
+                     <SelectItem value="4">Population (Descending)</SelectItem>
+                  </SelectGroup>
+               </SelectContent>
+            </Select>
+         </label>
+   )
+};
