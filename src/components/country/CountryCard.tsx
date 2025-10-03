83ce83a16d15e4599f899a13418dd99340feb030
dalowa <davidurbano.dev@gmail.com>
2025-10-03 10:06:12 -0500

    feat(ui): new way to see a country

--- src/components/country/CountryCard.tsx
+++ src/components/country/CountryCard.tsx
@@ -2,6 +2,7 @@ import { formatPopulation } from '@/lib/utils/format'
 import { Country } from '@/types/countriesAPI-type'
 import Image from 'next/image'
 import React from 'react'
+import { IconHeart } from '../icons'
 
 interface CountryCardProps {
    country: Country
@@ -16,11 +17,12 @@ export function CountryCard({country, setModalIsOpen, setCurrentModalCountry} : 
    }
   return (
     <div
-      className={`border border-gray-400 relative w-full md:w-52 xl:w-80 px-5 py-8 justify-between flex flex-col items-start rounded-lg cursor-pointer`}
+      className={`relative w-full max-w-xs justify-between flex bg-white flex-col items-start rounded-2xl overflow-hidden cursor-pointer
+border border-gray-300 shadow-lg hover:shadow-xl hover:-translate-y-1 transition-all duration-300 shadow-gray-400`}
       onClick={() => handleClickModal(country)}
    >
       
-      <div className='w-20 h-12 relative overflow-hidden'>
+      <div className='w-full h-40 relative overflow-hidden'>
          <Image 
             src={country.flags.png ?? country.flags.svg}
             alt={`Country flag of ${country.name.common}`}
@@ -28,10 +30,11 @@ export function CountryCard({country, setModalIsOpen, setCurrentModalCountry} : 
             className=""
          />
       </div>
-      <div className="justify-evenly w-full [&>p]:my-2">
-         <p className="font-bold text-start text-xl tracking-wider">{country.name.common}</p>
-         <p className="text-sm text-start md:text-base xl:text-xl px-2 bg-gray-300 rounded-lg inline">{`${country.region}`}</p>
-         <p className="text-sm text-start md:text-base xl:text-xl">{`Aprox. population: ${formatPopulation(country.population)}`}</p>
+      <div className="justify-evenly relative w-full [&>p]:my-1 p-4">
+         <p className="text-sm text-start px-3 py-1 bg-red-100 text-red-400 font-semibold  rounded-lg inline">{`${country.region}`}</p>
+         <p className="font-bold text-start text-2xl tracking-wider">{country.name.common}</p>
+         <p className="text-sm text-start md:text-base text-gray-600">{`Population: ${formatPopulation(country.population)}`}</p>
+         <IconHeart className='absolute w-6 h-6 top-4 right-4 text-gray-400 hover:text-red-400 transition-colors duration-200' />
       </div>
    </div>
   )
