99d39b325af83442775daf6a23114a9dd601a786
dalowa <davidurbano.dev@gmail.com>
2025-10-03 13:45:50 -0500

    refactor(state): using zustand now

--- src/components/country/CountryCardsDisplay.tsx
+++ src/components/country/CountryCardsDisplay.tsx
@@ -1,46 +1,44 @@
 'use client'
-
-import { Country } from '@/types/countriesAPI-type'
+import { useEffect } from 'react'
 import { CountryCard, CountryModal } from '../country'
-import { useCountries, useCountryFilters, useModalCountry,usePagination } from '@/hooks'
+import { useCountriesStore } from '@/store'
 
 export const CountryCardsDisplay = () => {
-  const { allCountries, loading } = useCountries()
-  const {...props} = useCountryFilters(allCountries)
-  const {currentModalCountry, isModalOpen, setCurrentModalCountry, setIsModalOpen} = useModalCountry()
-  const {currentItems, handleNext, handlePrevious, currentPage, totalPages} = usePagination(8,props.filteredCountries)
-  return (
-      <>
-         
-         <div className="flex flex-wrap max-w-[var(--max-w-container)] gap-4 px-5 justify-center">
-            {loading ? (
-               <div className='font-bold'>CARGANDO...</div>
-            ) : props.filteredCountries?.length === 0 ? (
-               <div className='font-bold'>NO HAY RESULTADOS</div>
-            ) : (
-               currentItems.map((co, i) => (
-                  <CountryCard 
-                     setCurrentModalCountry={setCurrentModalCountry}
-                     setModalIsOpen={setIsModalOpen}  
-                     country={co}
-                     key={`${i}+${co.name.common}`} />
-               ))
-            )}
-         </div>
-         <div className=' flex justify-center gap-2'>
-            <button 
-            type='button' 
-            onClick={() => handlePrevious()}
-            className='border cursor-pointer hover:bg-black hover:text-white border-black py-2 px-3 w-28 text-center rounded-2xl font-bold'>Previo</button>
+   const { itemsPerPage,allCountries, loading,filteredCountries,handleNext, handlePrevious,isModalOpen, currentPage, totalPages, fetchCountries } = useCountriesStore()
+   useEffect(() => {
+      fetchCountries()
+   }, [fetchCountries])
 
-            <div className='border cursor-pointer border-white py-2 px-3 w-20 text-center rounded-2xl font-bold'>{`Page  ${currentPage}/${totalPages}`}</div>
-            <button 
-            type='button' 
-            onClick={() => handleNext()}
-            className='border cursor-pointer hover:bg-black hover:text-white border-black py-2 px-3 w-28 text-center rounded-2xl font-bold'>Siguiente</button>
-         </div>
-         
-         {isModalOpen ? (<CountryModal setIsModalOpen={setIsModalOpen} country={currentModalCountry as Country} />):<></>}
-      </>
-  )
+   return (
+         <>
+            
+            <div className="flex flex-wrap max-w-[var(--max-w-container)] gap-4 px-5 justify-center">
+               {loading ? (
+                  <div className='font-bold'>CARGANDO...</div>
+               ) : allCountries?.length === 0 ? (
+                  <div className='font-bold'>NO HAY RESULTADOS</div>
+               ) : (
+                  filteredCountries.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((co) => (
+                     <CountryCard  
+                        country={co}
+                        key={`${co.cca3}`} />
+                  ))
+               )}
+            </div>
+            <div className=' flex justify-center gap-2'>
+               <button 
+               type='button' 
+               onClick={() => handlePrevious()}
+               className='border cursor-pointer hover:bg-black hover:text-white border-black py-2 px-3 w-28 text-center rounded-2xl font-bold'>Previo</button>
+
+               <div className='border cursor-pointer border-white py-2 px-3 w-20 text-center rounded-2xl font-bold'>{`Page  ${currentPage}/${totalPages}`}</div>
+               <button 
+               type='button' 
+               onClick={() => handleNext()}
+               className='border cursor-pointer hover:bg-black hover:text-white border-black py-2 px-3 w-28 text-center rounded-2xl font-bold'>Siguiente</button>
+            </div>
+            
+            {isModalOpen ? (<CountryModal />):<></>}
+         </>
+   )
 }
